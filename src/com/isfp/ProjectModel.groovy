796d493fd4ea265d5302d5f0b9221ea57edf8a05
amr farrag <eng.ame.farrag@outlook.com>
2022-03-06 12:38:39 +0200

    change dir

--- src/com/isfp/ProjectModel.groovy
+++ src/com/isfp/ProjectModel.groovy
@@ -8,7 +8,8 @@ class ProjectModel implements Serializable {
     String appDirectory;
     ProjectModel(name){
         def jsonSlurper = new JsonSlurper()
-        def config = jsonSlurper.parse(new File('../resources/config.json'))
+        echo pwd
+        def config = jsonSlurper.parse(new File('../../resources/config.json'))
         this.projectName=name
         this.buildNode=config[name].buildNode
         this.buildDirectory=config[name].buildDirectory
