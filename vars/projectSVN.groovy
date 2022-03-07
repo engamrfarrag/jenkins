360dd357606b022b55c4eee2716211f48ee7ed87
amr farrag <eng.ame.farrag@outlook.com>
2022-03-07 17:41:51 +0200

    concat

--- vars/projectSVN.groovy
+++ vars/projectSVN.groovy
@@ -1,6 +1,6 @@
 
 def svnUpdate(){
- def svnfiles=sh (returnStdout : true , script :'cd ${configurationContext.project.srcDirectory}  && svn up').split('\n')
+ def svnfiles=sh (returnStdout : true , script :'cd $configurationContext.project.srcDirectory  && svn up').split('\n')
  if(svnfiles > 2){
      return true
  }
