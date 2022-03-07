3b25d9232584b4bd9c291efe14b55cfc8f77f541
amr farrag <eng.ame.farrag@outlook.com>
2022-03-07 17:30:17 +0200

    init static

--- src/com/isfp/configurationContext.groovy
+++ src/com/isfp/configurationContext.groovy
@@ -1,10 +1,9 @@
 package com.isfp
 
 class configurationContext {
-   static ProjectModel project   
+   static ProjectModel project  =new ProjectModel() 
    static setProject(config,projectName){
         def projectJSON= config[projectName]
-        def project = new ProjectModel()
         project.buildNode=projectJSON.buildNode
    }
  }
