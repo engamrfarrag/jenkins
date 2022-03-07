dc8b0250528c03525bc0df2ef4dddb58209a0b4d
amr farrag <eng.ame.farrag@outlook.com>
2022-03-07 17:29:15 +0200

    typo

--- src/com/isfp/configurationContext.groovy
+++ src/com/isfp/configurationContext.groovy
@@ -3,7 +3,7 @@ package com.isfp
 class configurationContext {
    static ProjectModel project   
    static setProject(config,projectName){
-        def projectJSON= config[projectname]
+        def projectJSON= config[projectName]
         def project = new ProjectModel()
         project.buildNode=projectJSON.buildNode
    }
