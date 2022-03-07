ad1d9c15eaf84a817ad377c21f591804cc69f1f9
amr farrag <eng.ame.farrag@outlook.com>
2022-03-07 17:27:26 +0200

    change structure again

--- src/com/isfp/configurationContext.groovy
+++ src/com/isfp/configurationContext.groovy
@@ -1,11 +1,8 @@
 package com.isfp
-import groovy.json.JsonSlurper
 
 class configurationContext {
    static ProjectModel project   
-   static setProject(projectname){
-        def jsonSlurper = new JsonSlurper()
-        def config = jsonSlurper.parseText(this.libraryResource("config.json"))
+   static setProject(config,projectName){
         def projectJSON= config[projectname]
         def project = new ProjectModel()
         project.buildNode=projectJSON.buildNode
--- vars/projectConfig.groovy
+++ vars/projectConfig.groovy
@@ -1,6 +1,8 @@
 import com.isfp.configurationContext
-
+import groovy.json.JsonSlurper
 def call(String projectname){
-        configurationContext.setProject(projectname)
+        def jsonSlurper = new JsonSlurper()
+        def config = jsonSlurper.parseText(this.libraryResource("config.json"))
+        configurationContext.setProject(config,projectname)
         return configurationContext.project
 }
