9cd23399e586761e4e3d742027fa052776f4a6b2
amr farrag <eng.ame.farrag@outlook.com>
2022-03-07 16:32:50 +0200

    change

--- src/com/isfp/configurationContext.groovy
+++ src/com/isfp/configurationContext.groovy
@@ -1,8 +1,13 @@
 package com.isfp
+import groovy.json.JsonSlurper
 
 class configurationContext {
    static ProjectModel project   
    static setProject(projectname){
-       project=projectFactory.createProject(projectname)
+        def jsonSlurper = new JsonSlurper()
+        def config = jsonSlurper.parseText(this.libraryResource("config.json"))
+        def projectJSON= config[projectname]
+        def project = new ProjectModel()
+        project.buildNode=projectJSON.buildNode
    }
  }
