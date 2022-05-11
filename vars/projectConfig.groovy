8854816cd8af27f44f7be023f1c001e02763596b
engamrfarrag <50185475+engamrfarrag@users.noreply.github.com>
2022-05-11 10:08:09 +0200

    Update projectConfig.groovy
    change config to build

--- vars/projectConfig.groovy
+++ vars/projectConfig.groovy
@@ -2,7 +2,7 @@ import com.isfp.configurationContext
 import groovy.json.JsonSlurper
 def call(String projectname){
         def jsonSlurper = new JsonSlurper()
-        def config = jsonSlurper.parseText(this.libraryResource("config.json"))
+        def config = jsonSlurper.parseText(this.libraryResource("build.json"))
         configurationContext.setProject(config,projectname)
         return configurationContext.project
 }
